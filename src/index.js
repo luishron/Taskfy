3ca721cdb292e344ddb8ec21bd9a68eba74eecce
Luis H Ron <luishron@gmail.com>
2022-12-15 13:52:35 -0300

    fix(Global): :fire: Remove imports that are not necessary.
    Mui font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
